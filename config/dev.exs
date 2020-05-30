2795466712bf67efba68983623cb9dc659418360
Taehwan Kim <freewebwithme777@protonmail.com>
2020-05-30 13:06:50 -0700

    remove assets folder and add production config files

--- config/dev.exs
+++ config/dev.exs
@@ -20,15 +20,7 @@ config :church_app, ChurchAppWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [
-    node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--mode",
-      "development",
-      "--watch-stdin",
-      cd: Path.expand("../assets", __DIR__)
-    ]
-  ]
+  watchers: []
 
 # ## SSL Support
 #
