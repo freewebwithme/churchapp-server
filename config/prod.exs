725e935ac4ba54b0c26349209dc4fe7bd4e985b5
Taehwan Kim <freewebwithme777@protonmail.com>
2020-06-16 22:03:27 -0700

    changed url in prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -11,9 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :church_app, ChurchAppWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "agile-stream-36927.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "churchapp-server.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
